60befb335d210852ba6acf29ededa823b90a3ec3
albertincx <albertincx@gmail.com>
2024-05-18 18:02:54 +0700

    new build source move, readme for info

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ https://learnlanguages.app/
 ![Screen](screen.png)
 
 ## Source
-https://github.com/albertincx/learnlanguages-react
+https://github.com/albertincx/react-pwa-apps
 
--- sw.js
+++ sw.js
@@ -1,1 +1,133 @@
-if(!self.define){let e,s={};const i=(i,n)=>(i=new URL(i+".js",n).href,s[i]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()})).then((()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e})));self.define=(n,r)=>{const o=e||("document"in self?document.currentScript.src:"")||location.href;if(s[o])return;let t={};const l=e=>i(e,o),d={module:{uri:o},exports:t,require:l};s[o]=Promise.all(n.map((e=>d[e]||l(e)))).then((e=>(r(...e),t)))}}define(["./workbox-3e911b1d"],(function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"assets/index-0d21f563.js",revision:null},{url:"assets/index-3c5c5f72.css",revision:null},{url:"assets/index-504b2119.js",revision:null},{url:"assets/index-e63eb810.css",revision:null},{url:"assets/workbox-window.prod.es5-c46a1faa.js",revision:null},{url:"index.html",revision:"91515fb46d09e3a992d40ede7a86658b"},{url:"favicon.svg",revision:"a67646d82efd2245283640803cf6bc9a"},{url:"manifest.webmanifest",revision:"7bd69b71c09fd4742de7a8c09c04b63b"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))}));
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-e264f298'], (function (workbox) { 'use strict';
+
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "assets/index-B4nAFpZy.js",
+    "revision": null
+  }, {
+    "url": "assets/index-BOORdGfV.css",
+    "revision": null
+  }, {
+    "url": "assets/index-C5KpxO26.js",
+    "revision": null
+  }, {
+    "url": "assets/index-Ck-x9-AQ.css",
+    "revision": null
+  }, {
+    "url": "assets/index-legacy-C4zQpCNW.js",
+    "revision": null
+  }, {
+    "url": "assets/index-legacy-DTUwWYfi.js",
+    "revision": null
+  }, {
+    "url": "assets/polyfills-DNt7E_7d.js",
+    "revision": null
+  }, {
+    "url": "assets/polyfills-legacy-B3slO07P.js",
+    "revision": null
+  }, {
+    "url": "assets/workbox-window.prod.es5-D_kkWWxm.js",
+    "revision": null
+  }, {
+    "url": "assets/workbox-window.prod.es5-legacy-BOodOgXd.js",
+    "revision": null
+  }, {
+    "url": "index.html",
+    "revision": "4ff814ea663664c9fb3c8cc428fc37ef"
+  }, {
+    "url": "favicon.png",
+    "revision": "4a7a8ff306f47dc57c4a2e5b106dc2bd"
+  }, {
+    "url": "icon-192.png",
+    "revision": "f2c43308084508689c9ed3193ad962cb"
+  }, {
+    "url": "icon-512.png",
+    "revision": "635e8b5a9e7f84e8d9fd832ef9a2ea50"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "743b5d664c31e39a2766704f80e3d9f2"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
+//# sourceMappingURL=sw.js.map
